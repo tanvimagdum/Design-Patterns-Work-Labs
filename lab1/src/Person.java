5ccd4b66d91448b9ba126cc72fb6557b1dba3e53
Tanvi Magdum <tanvimagdum@Tanvis-MacBook-Pro.local>
2022-10-11 17:08:19 -0400

    Third version of Lab 1

--- lab1/src/Person.java
+++ lab1/src/Person.java
@@ -40,4 +40,13 @@ public class Person {
     return this.lastName;
   }
 
+  /**
+   * Return the year of birth of this person.
+   *
+   * @return the year of birth of this person
+   */
+  public int getYearOfBirth() {
+    return this.yearOfBirth;
+  }
 }
+
